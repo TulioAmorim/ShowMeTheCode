f66e5c53feeb03aedd8b55ca26a8101d89d31e46
tulio <tulio.amorimm@gmail.com>
2025-11-22 20:59:05 -0300

    adiciona arquivos de exemplos de POO no contabil antigo

---
+++ src/exemplosPooAgilize.php
@@ -0,0 +1,15 @@
+<?php
+
+// Heranca
+// - Packages/Application/Agilize.NfseWebservice/Classes/Agilize/NfseWebservice/Layout/Ginfes03/City/Santos/Domain/Model/NfseFactory.php:16
+// - Packages/Application/Apimenti.Account/Classes/Apimenti/Account/Domain/Model/Company.php:102
+
+// Polimorfismo
+// - Packages/Application/Apimenti.Account/Classes/Apimenti/Account/Domain/Model/Company.php e Packages/Application/Apimenti.Account/Classes/Apimenti/Account/Domain/Model/Tax/AbstractTax.php
+//   -> Cada empresa instanciada, sera diferente, uma da outra, apesar de ter mesmos atributos, metodos.
+
+// Encapsulamento
+// - Packages/Application/Apimenti.Account/Classes/Apimenti/Account/Domain/Model/Company.php
+//   -> Atributos privados e protegidos, acessiveis apenas por quem metodos e por quem utiliza a entidade Company.
+// - Packages/Application/Apimenti.Account/Classes/Apimenti/Account/Domain/Model/Tax/AbstractTax.php
+//   -> Pode ser exemplificado como Henranca, com atributos privados e protegidos, acessiveis apenas por quem metodos e por quem herdar essa classe.
